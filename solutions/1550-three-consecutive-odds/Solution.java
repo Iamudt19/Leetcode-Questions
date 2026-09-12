b4b4abf7703d22e9263bc749acadc2ea81cc4e5d
Udit <iamudt19@GMAIL.COM>
2026-09-12 22:11:07 +0530

    LeetCode #1550 - Three Consecutive Odds [Java]

---
+++ solutions/1550-three-consecutive-odds/Solution.java
@@ -0,0 +1,13 @@
+
+        }
+        return false;
+                c--;
+            }
+            else{
+                c=3;
+            }
+            if(c==0){
+                return true;
+            }
+    }
+}
